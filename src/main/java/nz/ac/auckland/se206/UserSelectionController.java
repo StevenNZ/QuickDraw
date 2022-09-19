6316c26d19b1faaa4f6c5d34486df2ee7469bcf7
Sli521 <sli521@aucklanduni.ac.nz>
2022-09-19 17:34:48 +1200

    feat - clickable user profiles
    Implemented transparent buttons for each user story and will either, create user story or start game with that user

--- src/main/java/nz/ac/auckland/se206/UserSelectionController.java
+++ src/main/java/nz/ac/auckland/se206/UserSelectionController.java
@@ -2,17 +2,53 @@ package nz.ac.auckland.se206;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javax.swing.*;
+import nz.ac.auckland.se206.user.UserProfile;
 
 public class UserSelectionController {
+
+  private static final UserProfile[] users = new UserProfile[7];
+  private static int currentUser = 0;
+
   @FXML
   private void onStartCanvas(ActionEvent event) {
     Button button = (Button) event.getSource();
     Scene sceneOfButton = button.getScene();
     sceneOfButton.setRoot(SceneManager.getUiRoot(SceneManager.AppUi.CANVAS));
+  }
+
+  @FXML
+  private void onCreateProfile(ActionEvent event) {
+    String id = ((Node) event.getSource()).getId();
+    int idIndex = getProfile(id);
+    if (users[idIndex] == null) {
+      // Go to profile creation scene
+    } else {
+      currentUser = idIndex;
+      onStartCanvas(event);
+    }
+  }
+
+  private int getProfile(String id) {
+    switch (id) {
+      case "btnProfile1":
+        return 1;
+      case "btnProfile2":
+        return 2;
+      case "btnProfile3":
+        return 3;
+      case "btnProfile4":
+        return 4;
+      case "btnProfile5":
+        return 5;
+      case "btnProfile6":
+        return 6;
+      default:
+        return 0;
+    }
   }
 
   public void onBlackSelected(MouseEvent mouseEvent) {}
