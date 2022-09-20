8210fa36ca00db27345243e1472b97ae2a06e644
Sli521 <sli521@aucklanduni.ac.nz>
2022-09-20 21:55:49 +1200

    feat - canvas implemented in user creation pane
    Canvas drawing now implemented when creating use profile

--- src/main/java/nz/ac/auckland/se206/UserSelectionController.java
+++ src/main/java/nz/ac/auckland/se206/UserSelectionController.java
@@ -1,13 +1,17 @@
 package nz.ac.auckland.se206;
 
+import java.util.Arrays;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javax.swing.*;
 import nz.ac.auckland.se206.user.UserProfile;
 
@@ -16,11 +20,22 @@ public class UserSelectionController {
   @FXML private Pane paneUserProfile;
   @FXML private Pane paneUserCreation;
   @FXML private TextField textFieldName;
+  @FXML private Canvas canvasUser;
+  private GraphicsContext graphic;
   private static final UserProfile[] users = new UserProfile[7];
   public static int currentUser = 0;
 
   @FXML
+  public void initialize() {
+    users[0] = new UserProfile("Guest");
+    // this is where we show locally stored user profiles
+  }
+
+  @FXML
   private void onStartCanvas(ActionEvent event) {
+    System.out.println("canvas" + currentUser);
+    System.out.println(Arrays.toString(users));
+
     Button button = (Button) event.getSource();
     Scene sceneOfButton = button.getScene();
     sceneOfButton.setRoot(SceneManager.getUiRoot(SceneManager.AppUi.CANVAS));
@@ -30,9 +45,20 @@ public class UserSelectionController {
   private void onCreateProfile(Event event) {
     currentUser = getProfile(event);
 
-    if (currentUser == 0) {
-      users[0] = new UserProfile("Guest");
-    }
+    graphic = canvasUser.getGraphicsContext2D();
+
+    canvasUser.setOnMouseDragged(
+        e -> {
+          // Brush size (you can change this, it should not be too small or too large).
+          final double size = 12.0;
+
+          final double x = e.getX() - size / 2;
+          final double y = e.getY() - size / 2;
+
+          // This is the colour of the brush.
+          graphic.setFill(Color.BLACK);
+          graphic.fillOval(x, y, size, size);
+        });
 
     paneUserProfile.setVisible(false);
     paneUserCreation.setVisible(true);
