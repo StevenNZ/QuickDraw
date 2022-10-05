59bc3720af905b29888c7a6236930b9d6030edd6
Hugo20038 <hmil830@aucklanduni.ac.nz>
2022-10-05 17:05:38 +1300

    Feat - Changed category getter function to private and usable for different categories.

--- src/main/java/nz/ac/auckland/se206/CategorySelector.java
+++ src/main/java/nz/ac/auckland/se206/CategorySelector.java
@@ -8,23 +8,28 @@ import java.util.ArrayList;
 
 public class CategorySelector {
 
-  public static ArrayList<String> getEasyWords() throws CsvValidationException, IOException {
+  private static ArrayList<String> getWords(String category) {
 
-    CSVReader fileReader =
-        new CSVReader(
-            new FileReader(
-                "src/main/resources/csv/category_difficulty.csv")); // reader for the csv file
     ArrayList<String> categories = new ArrayList<String>();
     String[] nextLine;
 
-    while ((nextLine = fileReader.readNext())
-        != null) { // while the next entry in the file isnt null
-      if (nextLine[1].equals("E")) { // if the category is classed easy
-        categories.add(nextLine[0]); // then add it to the list to be chosen from
-      }
-    }
+    try {
+      CSVReader fileReader =
+          new CSVReader(
+              new FileReader(
+                  "src/main/resources/csv/category_difficulty.csv")); // reader for the csv file
 
-    fileReader.close();
+      while ((nextLine = fileReader.readNext())
+          != null) { // while the next entry in the file isnt null
+        if (nextLine[1].equals(category)) { // if the category is classed easy
+          categories.add(nextLine[0]); // then add it to the list to be chosen from
+        }
+      }
+
+      fileReader.close();
+    } catch (CsvValidationException | IOException e) {
+      e.printStackTrace();
+    }
     return categories;
   }
 }
