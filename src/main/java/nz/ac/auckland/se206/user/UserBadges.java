09499fbfd2135ee69a740b8586c189b414d3ff56
Hugo20038 <hmil830@aucklanduni.ac.nz>
2022-10-11 00:03:15 +1300

    Feat - Added the UserBadges class with badge names.

---
+++ src/main/java/nz/ac/auckland/se206/user/UserBadges.java
@@ -0,0 +1,18 @@
+package nz.ac.auckland.se206.user;
+
+public class UserBadges {
+  private boolean twentySecondWin = false;
+  private boolean tenSecondWin = false;
+  private boolean fiveSecondWin = false;
+  private boolean threeWinstreak = false;
+  private boolean sixWinstreak = false;
+  private boolean tenWinstreak = false;
+  private boolean fiveMasterWins = false;
+  private boolean fifteenMasterWins = false;
+  private boolean thirtyMasterWins = false;
+  private boolean fiveHiddenWins = false;
+  private boolean fifteenHiddenWins = false;
+  private boolean thirtyHiddenWins = false;
+
+  public UserBadges() {}
+}
