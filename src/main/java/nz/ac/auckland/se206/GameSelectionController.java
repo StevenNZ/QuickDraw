4247dc0aa02988f8ac2d37d36f3663b90c20cc82
Hugo20038 <hmil830@aucklanduni.ac.nz>
2022-10-15 11:20:47 +1300

    Feat - Game selector resets when exiting out/

--- src/main/java/nz/ac/auckland/se206/GameSelectionController.java
+++ src/main/java/nz/ac/auckland/se206/GameSelectionController.java
@@ -98,6 +98,11 @@ public class GameSelectionController {
 
   @FXML
   private void onBack(Event event) {
+    paneModes.setDisable(false);
+    paneModes.setVisible(true);
+    normalModeSelection.setDisable(true);
+    normalModeSelection.setVisible(false);
+
     Button button = (Button) event.getSource();
     Scene sceneOfButton = button.getScene();
     sceneOfButton.setRoot(SceneManager.getUiRoot(SceneManager.AppUi.MAINMENU));
