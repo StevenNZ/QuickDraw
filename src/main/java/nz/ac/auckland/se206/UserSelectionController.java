13b27c57d1171c4edd0ea7e823a8457a367d936f
Sli521 <sli521@aucklanduni.ac.nz>
2022-10-15 21:06:47 +1300

    feature - profile picture can be saved in colour
    changed buffer image type to RGB

--- src/main/java/nz/ac/auckland/se206/UserSelectionController.java
+++ src/main/java/nz/ac/auckland/se206/UserSelectionController.java
@@ -245,7 +245,7 @@ public class UserSelectionController {
 
     // Convert into a binary image.
     final BufferedImage imageBinary =
-        new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+        new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
 
     final Graphics2D graphics = imageBinary.createGraphics();
 
