51ecb81aaffcefedc7923ea66dd3fb4ec57c1b97
Hugo20038 <hmil830@aucklanduni.ac.nz>
2022-10-25 16:16:46 +1300

    Feat - Sets currently established fields.

--- src/main/java/nz/ac/auckland/se206/StatisticsController.java
+++ src/main/java/nz/ac/auckland/se206/StatisticsController.java
@@ -1,11 +1,48 @@
 package nz.ac.auckland.se206;
 
+
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import nz.ac.auckland.se206.user.UserProfile;
 
 public class StatisticsController {
+
+  @FXML Label lblWinRate;
+  @FXML Label lblQuickestWin;
+  @FXML Label lblTotalGames;
+  @FXML Label lblNormalWins;
+  @FXML Label lblNormalLosses;
+  @FXML Label lblNormalQuickest;
+  @FXML Label lblHiddenWins;
+  @FXML Label lblHiddenLosses;
+  @FXML Label lblHiddenQuickest;
+  private UserProfile currentUserProfile;
+  private String lblText;
+
+  public void initialize() {
+    currentUserProfile = UserSelectionController.users[UserProfile.currentUser];
+
+    if (currentUserProfile.getTotalWins() != 0 || currentUserProfile.getTotalLoss() != 0) {
+      lblText =
+          String.valueOf(
+              (double) currentUserProfile.getTotalWins()
+                  / ((double) currentUserProfile.getTotalWins()
+                      + currentUserProfile.getTotalLoss()));
+      lblText = lblText.substring(2, 4) + "%";
+      lblWinRate.setText(lblText);
+    }
+
+    if (currentUserProfile.getQuickestWin() != 100) {
+      lblText = String.valueOf(currentUserProfile.getQuickestWin()) + "s";
+      lblQuickestWin.setText(lblText);
+    }
+
+    lblText = String.valueOf(currentUserProfile.getTotalLoss() + currentUserProfile.getTotalWins());
+    lblTotalGames.setText(lblText);
+  }
 
   @FXML
   private void onBack(Event event) {
