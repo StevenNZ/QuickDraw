48a1f4bd49ec9b8c8fd98d517871086edbebf34a
Sli521 <sli521@aucklanduni.ac.nz>
2022-10-28 20:26:56 +1300

    feature - implemented toggle music method
    also reduced noise

--- src/main/java/nz/ac/auckland/se206/MainMenuController.java
+++ src/main/java/nz/ac/auckland/se206/MainMenuController.java
@@ -13,6 +13,14 @@ public class MainMenuController {
 
   protected static Clip clip;
 
+  protected static void toggleMusic() {
+    if (clip.isRunning()) {
+      clip.stop();
+    } else {
+      clip.loop(Integer.MAX_VALUE);
+    }
+  }
+
   public void initialize()
       throws UnsupportedAudioFileException, IOException, LineUnavailableException {
     String file = "/sounds/backgroundMusic.wav";
@@ -20,6 +28,14 @@ public class MainMenuController {
         AudioSystem.getAudioInputStream(this.getClass().getResource(file));
     clip = AudioSystem.getClip();
     clip.open(audioInputStream);
+    FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+    float range = gainControl.getMaximum() - gainControl.getMinimum();
+    float gain = (range * 0.68f) + gainControl.getMinimum();
+    gainControl.setValue(gain);
+    try {
+      clip.wait(4000);
+    } catch (Exception ignored) {
+    }
     clip.loop(Integer.MAX_VALUE);
   }
 
