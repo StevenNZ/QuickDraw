c82874c097139209adfa8cf2eab82ae8a4fa5848
Sli521 <sli521@aucklanduni.ac.nz>
2022-10-28 19:31:47 +1300

    feature - implemented an audio cip that loops the music
    current initializes on main menu

--- src/main/java/nz/ac/auckland/se206/MainMenuController.java
+++ src/main/java/nz/ac/auckland/se206/MainMenuController.java
@@ -1,13 +1,27 @@
 package nz.ac.auckland.se206;
 
+import java.io.IOException;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javax.sound.sampled.*;
 
 /** Controls the Main Menu which acts as a landing page for the game. */
 public class MainMenuController {
+
+  protected static Clip clip;
+
+  public void initialize()
+      throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+    String file = "/sounds/backgroundMusic.wav";
+    AudioInputStream audioInputStream =
+        AudioSystem.getAudioInputStream(this.getClass().getResource(file));
+    clip = AudioSystem.getClip();
+    clip.open(audioInputStream);
+    clip.loop(Integer.MAX_VALUE);
+  }
 
   /** On exit, it exits the application fully. Not much more complicated than that. */
   @FXML
